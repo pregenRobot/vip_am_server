aded6fd299c68dca5add3ef55bd9572bac89ac60
Tamim Azmain <tamim.azmain@rooter.co.jp>
2021-08-08 02:41:53 +0900

    added validate source create request

--- server/app/controllers/Source.controller.js
+++ server/app/controllers/Source.controller.js
@@ -13,16 +13,67 @@ exports.findTopTen = (req, res) => {
 exports.create = (request, response) => {
 
 	const {
+		fileName: fileName,
+		filePath: filePath,
+		fileSize: fileSize,
+		fileHash: fileHash,	
+		fileDuration: fileDuration,
+		dataStorageLocation: dataStorageLocation,
+		channelCount: channelCount,
+		samplingRate: samplingRate,
+		description: description,
+	} = request.body;
+
+	const source = new Source({
 		fileName,
 		filePath,
 		fileSize,
-		fileType,
 		fileHash,
 		fileDuration,
 		dataStorageLocation,
 		channelCount,
 		samplingRate,
 		description,
-		
-	} = request.body;
+	});
+
+	const validationResult = validateCreate(source);
+}
+
+
+const validateCreate = (source) => {
+	const errors = [];
+
+	if (!source.fileName) {
+		errors.push("Missing file name");
+	}
+	if (!source.filePath) {
+		errors.push("Missing file path");
+	}
+	if (!source.fileSize) {
+		errors.push("Missing file size");
+	}
+	if (!source.fileHash) {
+		errors.push("Missing file hash");
+	}
+	if (!source.fileDuration) {
+		errors.push("Missing file duration");
+	}
+	if (!source.dataStorageLocation) {
+		errors.push("Missing data storage location");
+	}
+	if (!source.channelCount) {
+		errors.push("Missing channel count");
+	}
+	if (!source.samplingRate) {
+		errors.push("Missing sampling rate");
+	}
+	if (!source.description) {
+		errors.push("Missing description");
+	}
+	
+	return {
+		message: errors.length == 0 ? "Successfully added a source" : "Invalid create source request",
+		errors: errors,
+		success: errors.length == 0 ? true : false,
+	}
 }
